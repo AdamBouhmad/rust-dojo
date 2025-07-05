e328d05006091ae14a07b90024151a489459a66c
AdamBouhmad <adbouhmad@gmail.com>
2025-07-05 00:29:14 -0400

    concurrently handle two futures

--- async_spawn/src/log_events.rs
+++ async_spawn/src/log_events.rs
@@ -5,9 +5,10 @@ pub async fn log_event(status: &str) {
     let mut sleep_timer = 2;
 
     if status == "startup" {
-        let sleep_timer = 5;
+        sleep_timer = 5;
     }
 
+    println!("{sleep_timer}");
     sleep(Duration::from_secs(sleep_timer)).await;
 
     println!("Event completed: <{status}>")
--- async_spawn/src/main.rs
+++ async_spawn/src/main.rs
@@ -1,12 +1,20 @@
 mod log_events;
 use tokio::spawn;
+use tokio::join;
 
 #[tokio::main]
 async fn main() {
 
-    spawn(log_events::log_event("startup"));
-    spawn(log_events::log_event("shutdown"));
+    let startup_event = spawn(log_events::log_event("startup"));
+    let shutdown_event = spawn(log_events::log_event("shutdown"));
 
     println!("Both tasks have been launched");
+
+    //awaiting one by one, no concurrency 
+        //startup_event.await.unwrap();
+        //shutdown_event.await.unwrap();
+
+    //concurrent awaiting both handles
+    let _ = join!(startup_event, shutdown_event);
     
 }
